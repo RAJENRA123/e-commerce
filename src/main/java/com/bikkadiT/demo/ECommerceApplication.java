fbd69e2d806937194e2b58f4531ebca6ef989871
Your Name <you@example.com>
2023-06-17 21:28:54 +0530

    UserController Implemented

--- src/main/java/com/bikkadiT/demo/ECommerceApplication.java
+++ src/main/java/com/bikkadiT/demo/ECommerceApplication.java
@@ -3,13 +3,18 @@ package com.bikkadiT.demo;
 import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(ECommerceApplication.class, args);
+		System.out.println("running up");
 	}
 
 
 }
+sout
