b6f4bd356e195ef986bd907c7986c9fb0d92f3c0
Your Name <you@example.com>
2023-06-19 16:59:17 +0530

    application.properties update

---
+++ src/main/java/com/bikkadiT/demo/dtos/FileService.java
@@ -0,0 +1,22 @@
+package com.bikkadiT.demo.dtos;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+
+@Service
+public class FileService {
+
+    public static String uploadFile(MultipartFile image, String imagePath) {
+        return imagePath;
+    }
+
+
+    public InputStream getResource(String imagePath) {
+        return InputStream.nullInputStream();
+    }
+}
+
+
